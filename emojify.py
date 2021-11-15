dc5f462f82beb7492b4e4592608b1a03ee7fa977
Aron P <poczaaron@gmail.com>
2021-11-15 21:45:28 +0100

    English sentences are fine

---
+++ emojify.py
@@ -0,0 +1,26 @@
+from translate import Translator
+from advertools import emoji_search
+from random import randrange
+
+import advertools
+import translate
+
+translator = translate.Translator(from_lang='hu', to_lang='en', provider='libre')
+
+def emojizeWord(s: str) -> str:
+    emojis = emoji_search(s).emoji
+    try:
+        return emojis[randrange(0, emojis.size)]
+    except:
+        return advertools.emoji_df.emoji[randrange(0, advertools.emoji_df.emoji.size)]
+
+def emojizeSentence(text: str) -> str:
+    emojitext = ""
+    for word in text.split():
+        print(word)
+        emojitext += str(word) + " " + emojizeWord(word) + " "
+    return emojitext
+
+def internationalEmojizer(text: str) -> str:
+    translated_text= translator.translate(text)
+    return translated_text
---
+++ main.py
@@ -0,0 +1,20 @@
+from advertools import emoji, emoji_search
+from random import randrange
+
+import advertools
+
+import emojify
+
+# print('hallo')
+
+# dogs = emoji_search('dog').emoji
+# print(dogs.size)
+# print(emoji_search('dog').emoji)
+# print(emoji_search('dog').emoji)
+
+# print(emojify.emojizeWord('kaka'))
+
+# print(advertools.emoji_df.emoji.size)
+
+# print(emojify.emojizeSentence("I run with the dog on the beach"))
+print(emojify.internationalEmojizer("Elmentem a kutyával a strandra futni"))
